0b2952fffc9e15ddb3f2e364a24f953afb13593e
Nerias <49833175+Nerias@users.noreply.github.com>
2020-08-24 09:32:17 +0700

    Chuong trinh C co ban

--- Code C/Thuc hanh/16.2 Xac dinh 1 so co phai so nguyen to khong.c
+++ Code C/Thuc hanh/16.2 Xac dinh 1 so co phai so nguyen to khong.c
@@ -1,21 +1,28 @@
 //Viet chuong trinh tim so nguyen to
 #include<stdio.h>
 #include<conio.h>
-int main() {
+void prime() {
 	int n,i;
 	printf("Enter a value:");
 	scanf("%d",&n);
 	if (n < 2) {
-		printf("%d khong phai so nguyen to",n);
+		printf("%d khong phai so nguyen to\n",n);
+		return;
 	}
 	if (n == 2){
 		printf("%d la so nguyen to\n",n);
+		return;
 	}
 	for (i = 2; i <= n/2; i++) { // n/2 vi 1 so khong bao gio chia het cho 1 so lon hon 1/2 cua no
 		if (n % i == 0){
 			printf("%d khong la so nguyen to\n",n);
-			return 0;
+			return;
 		}
 	}
 	printf("%d la so nguyen to\n",n);
+}
+int main() {
+	while (1>0) {
+		prime();
+	}
 }
