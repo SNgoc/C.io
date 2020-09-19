b9fa43581573f3f3858972809642792cf07d9df9
Nerias <49833175+Nerias@users.noreply.github.com>
2020-09-20 02:40:29 +0700

    Chương trình C cơ bản

--- Code C/Thuc hanh/0.Tets.c
+++ Code C/Thuc hanh/0.Tets.c
@@ -1,42 +1,12 @@
-// Viet chuong trinh nhap so cong van den va xuat ra so cong van bat ky khi duoc yeu cau
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<string.h>
-int main() {
-	struct employee {
-		int empID;
-		char empName[100];
-		int salary, allowance;
-		int totalsalary;
-	}arrE[100];
-	int numE,i;
-	printf("Enter the number of employees: ");
-	scanf("%d",&numE);
-	for (i=1; i<=numE; i++) 
-	{
-		printf("\nEnter information employees %d:\n",i);
-		fflush(stdin);
-		printf("Input ID: ");
-		scanf("%d",&arrE[i].empID);
-		printf("Input Name: ");
-		fflush(stdin);
-		gets(arrE[i].empName);
-		printf("Input Salary: ");
-		scanf("%d",&arrE[i].salary);
-		printf("Input Allowance: ");
-		scanf("%d",&arrE[i].allowance);
-	}
-	//Yeu cau 2
-	printf("\n---------------------Employee Details---------------------\n");
-	printf("ID \tName \t\t\t\tSalary \tAllowance \tTotal Salary\n");
-	for (i=1; i<=numE; i++)  
-	{
-		arrE[i].totalsalary = arrE[i].salary + arrE[i].allowance;
-		printf("\r%d",arrE[i].empID);// \r de can le tr, \t phai printf tung phan de can dong
-		printf("\t%-20s",arrE[i].empName);// %-20s de can le phai
-		printf("\t%d",arrE[i].salary);
-		printf("\t%d",arrE[i].allowance);
-		printf("\t%d\n",arrE[i].totalsalary);
-	}
+// Test ly thuyet
+#include <stdio.h>
+#include <conio.h>
+main()
+{	int i=0, x=0;
+	while ( i < 20)
+	{	if (i%5==0)
+		x = x+i;
+		++i;  }
+		printf("%d",x);
 }
+
